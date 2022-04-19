e20f388937664cbaa96b8ed84882d6a8e133844e
James Wu <jingswu@amazon.com>
2022-04-19 15:27:18 +0000

    initial commit

---
+++ processing/docker/video_processing.py
@@ -0,0 +1,168 @@
+import time
+import logging
+import argparse
+
+import cv2
+import boto3
+from botocore.exceptions import ClientError
+from sagemaker.predictor import Predictor
+import sagemaker, json
+from sagemaker import get_execution_role
+
+from opensearchpy import OpenSearch, RequestsHttpConnection, AWSV4SignerAuth
+
+import octank_utility as utility
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
+
+#initialize general info
+input_path = "/opt/ml/processing/input"
+output_path = '/opt/ml/processing/output'
+
+region = 'us-east-1'
+sess = sagemaker.Session(boto3.session.Session(region_name=region))
+
+
+#initialize rekognition service
+rekognition = boto3.client('rekognition', region_name=region)
+
+project_arn='arn:aws:rekognition:us-east-1:913089978341:project/retail-test-2/1648085376750'
+model_arn='arn:aws:rekognition:us-east-1:913089978341:project/retail-test-2/'+\
+            'version/retail-test-2.2022-03-23T20.40.12/1648086013117'
+
+version_name='retail-test-2.2022-03-23T20.40.12'
+
+# initialize SageMaker endpoint
+embedding_endpoint = 'jumpstart-example-infer-tensorflow-icem-2022-04-14-15-14-03-830'
+# embedding model predictor
+model_predictor = Predictor(endpoint_name = embedding_endpoint,
+                           sagemaker_session = sess)
+
+#initialize opensearch
+opensearch_domain_endpoint = 'search-image-search-2-5citm4637xvf5ufci35pbb6g4u.us-east-1.es.amazonaws.com'
+
+index_name = 'image_embedding'
+
+credentials = boto3.Session().get_credentials()
+auth = AWSV4SignerAuth(credentials, region)
+
+opensearch_host = {
+    'host' : opensearch_domain_endpoint,
+    'port' : 443,
+    'scheme' : 'https',
+}
+
+opensearch = OpenSearch(hosts = [opensearch_host],
+               http_auth = auth,
+                use_ssl = True,
+                verify_certs = True,
+                connection_class = RequestsHttpConnection)
+
+#initialize DynamoDB
+table_name = 'octank_movie'
+dynamodb = boto3.resource('dynamodb', region_name=region)
+table = dynamodb.Table(table_name)
+
+def process_frames(frame):
+    
+    # get width and height info for each frame
+    height = frame.shape[0]
+    width = frame.shape[1]
+    
+    # send frame to Rekognition for inference
+    logger.debug("Run product detection =============")
+
+    image_bytes = cv2.imencode('.jpg', frame)[1].tobytes()
+    results = utility.analyze_video_frame(rekognition, 
+                                 model_arn, 
+                                image_bytes,
+                                80)
+    
+    detections = []
+    for product in results:
+        
+        detect =dict()
+
+        # crop images
+        logger.debug("Crop detected product =============")
+        cropped, bbox = utility.crop_images(frame, product['Geometry']['BoundingBox'])
+    
+        detect['bbox']=bbox
+
+        cropped_bytes = cv2.imencode('.jpeg', cropped)[1].tobytes()
+
+        # generate embedding
+        logger.debug("Generate embedding =============")
+        embedding_vector = utility.generate_embedding(cropped_bytes, model_predictor)
+        
+        # search product info from OpenSearch
+        logger.debug("Search Query =============")
+        results = utility.knn_search(opensearch, index_name, 1, embedding_vector)
+        
+        detect['product_id'] = results[0][1]
+        detect['product_category'] = results[0][2]
+        
+        detections.append(detect)
+    
+    
+    return detections
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--filename", type=str, default="")
+    parser.add_argument("--start", type=int, default=0)
+    args, _ = parser.parse_known_args()
+
+    
+    # check if rekognition model is ready
+    ready = utility.check_rekognition_model_status(rekognition,
+                                                   project_arn,
+                                                   version_name)
+    
+    if ready:
+        output = dict()
+        
+        output['video_id'] = args.filename
+        
+        video_path = f"{input_path}/{args.filename}"
+        
+        print(video_path)
+        output['detections'] = dict()
+        
+        # load the movie video
+        cap = cv2.VideoCapture(video_path)
+
+        frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        logger.info(f"Total frame count: {frame_count} =============" )
+
+        
+        frame_id = args.start
+        cap.set(cv2.CAP_PROP_POS_FRAMES, frame_id)
+        
+        try:
+            while True:
+                logger.debug(f"processing frame number {frame_id} ============")
+                _, frame = cap.read()
+
+                detections = process_frames(frame)
+
+                for d in detections:
+                    frame = utility.draw_bbox(cv2, frame, d['bbox'])
+
+                output['detections'][str(frame_id)] = detections
+                frame_id += 1
+                if frame_id >= frame_count:
+                    break
+                    
+            logger.debug("Upload output data to DynamoDB ==========")
+            table.put_item(
+                Item=output
+            )
+        except KeyboardInterrupt:
+            pass
+        finally:
+            cap.release()
+        
+    logger.info("Finished running processing job")
---
+++ processing/src/octank_utility.py
@@ -0,0 +1,128 @@
+import logging
+from botocore.exceptions import ClientError
+import json
+
+logger = logging.getLogger(__name__)
+
+# This utility function checks if a rekognition model is ready to use
+def check_rekognition_model_status(client, project_arn, version_name):
+    try:
+        describe_response = client.describe_project_versions(
+                        ProjectArn=project_arn,
+                        VersionNames=[version_name])
+        
+        if len(describe_response['ProjectVersionDescriptions'])<1:
+            logger.error("No Rekognition mode found...")
+            return False
+        
+        #Get model status
+        model = describe_response['ProjectVersionDescriptions'][0]
+        status = model['Status']
+        
+        if status != "RUNNING":
+            logger.error("Rekognition Model Not Running...")
+            return False
+    
+    except ClientError as client_err:
+        logger.error(format(client_err))
+        return False
+    
+    return True
+
+
+# This is a utility function to call rekognition model and run inference on a image
+def analyze_video_frame(rek_client, model_arn, frame, min_confidence):
+
+    try:
+
+        response = rek_client.detect_custom_labels(Image={'Bytes': frame},
+            MinConfidence=min_confidence,
+            ProjectVersionArn=model_arn)
+        
+        return response['CustomLabels']
+
+    except ClientError as client_err:
+        logger.error(format(client_err))
+        raise
+        
+# This function crops out a bbox from an image
+def crop_images(frame, bbox):
+    
+    bbox_new = dict()
+    
+    height = frame.shape[0]
+    width = frame.shape[1]
+
+    bbox_w = int(bbox['Width']*width)
+    bbox_h = int(bbox['Height']*height)
+    bbox_l = int(bbox['Left']*width)
+    bbox_t = int(bbox['Top']*height)
+    
+    bbox_new['top'] = bbox_t
+    
+    bbox_new['bottom'] = bbox_t+bbox_h
+    
+    bbox_new['left'] = bbox_l
+    
+    bbox_new['right'] = bbox_l+bbox_w
+
+    cropped = frame[bbox_t:bbox_t+bbox_h, bbox_l:bbox_l+bbox_w]
+    return cropped, bbox_new
+
+# This function extracts the embedding results from a Sagemaker endpoint
+def parse_response(query_response):
+    """Parse response and return the embedding."""
+
+    model_predictions = json.loads(query_response)
+    translation_text = model_predictions["embedding"]
+    return translation_text
+
+# This function calls a sagemaker endpoint to generate embedding
+def generate_embedding(img_bytes, model_predictor):
+    
+    query_response = model_predictor.predict(
+        img_bytes,
+        {
+            "ContentType": "application/x-image",
+            "Accept": "application/json",
+        },
+    )
+    
+    vector = parse_response(query_response)
+    return vector
+
+# This function calls opensearch to get knn search results
+def knn_search(opensearch, index_name, k, embedding_vector):
+    query = {
+        "size":k,
+        "query": {
+            "knn": {
+                "embedding": {
+                    "vector": embedding_vector,
+                    "k": k
+                }
+            }
+        }
+    }
+    
+    res = opensearch.search(index = index_name, body=query)
+
+    results = []
+    for r in res["hits"]["hits"]:
+        results.append([r["_score"], r["_source"]["product_id"]])
+    
+    return results
+
+#This function draws bbox on images
+def draw_bbox(cv2, frame, bbox):
+    
+    top = bbox['top']
+    left = bbox['left']
+    bottom = bbox['bottom']
+    right = bbox['right']
+    
+    cv2.rectangle(frame, (left, top), (right, bottom), (0, 165, 255), 10)
+    
+    return frame
+    
+    
